c2042857ef79180f9531aa290d1509a5fb48b536
Dominik Alencinovič <s057152@ad.viko.lt>
2024-09-12 11:43:11 +0300

    pirmas projektas

--- 1projektas/main.cpp
+++ 1projektas/main.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << std::endl;
+int main()
+{
+    //1uzd
+    string varda = "Dominik", pavarde = "Alencinovic", grupe="PI24",studiju_pav="Programu Sistemos";
+    int amzius = 18,kursas=1;
+
+    cout<<"Vardas: "<<varda<<endl;
+    cout<<"Pavarde: "<<pavarde<<endl;
+    cout<<"Amzius: "<<amzius<<endl;
+    cout<<"Grupe: "<<grupe<<endl;
+    cout<<"Kursas: "<<kursas<<endl;
+    cout<<"Studiju programos pavadinimas: "<<studiju_pav<<endl;
+
+    //2uzd
+
+    string pavadinimas="Madrido Real",savininkas="....",arena=".....";
+    int metai=1902, vietu= 999;
+
+    cout<<"Pavadinimas: "<<pavadinimas<<endl;
+    cout<<"Ikurimo metai: "<<metai<<endl;
+    cout<<"Savininkas: "<<savininkas<<endl;
+    cout<<"Arena: "<<arena<<endl;
+    cout<<"Vietu skaicius: "<<vietu<<endl;
+
+    //3uzd
+
+    string marke="Audi", modelis="c6",spalva="pilka";
+    int metais=2007,litrazas=3;
+
     return 0;
 }
+
