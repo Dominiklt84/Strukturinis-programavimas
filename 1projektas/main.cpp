5c073db09a6ee15e43f81e732e09e113c5ee909c
Dominik Alencinovič <s057152@ad.viko.lt>
2024-09-13 09:06:05 +0300

    parasiau pirma projekta

--- 1projektas/main.cpp
+++ 1projektas/main.cpp
@@ -14,9 +14,9 @@ int main()
     cout<<"Grupe: "<<grupe<<endl;
     cout<<"Kursas: "<<kursas<<endl;
     cout<<"Studiju programos pavadinimas: "<<studiju_pav<<endl;
+    cout<<" "<<endl;
 
     //2uzd
-
     string pavadinimas="Madrido Real",savininkas="....",arena=".....";
     int metai=1902, vietu= 999;
 
@@ -25,14 +25,39 @@ int main()
     cout<<"Savininkas: "<<savininkas<<endl;
     cout<<"Arena: "<<arena<<endl;
     cout<<"Vietu skaicius: "<<vietu<<endl;
+    cout<<" "<<endl;
 
     //3uzd
-
-    string marke="Audi", modelis="c6",spalva="pilka";
+    string marke="Audi", modelis="c6",spalva="juodos";
     int metais=2007;
     float litrazas= 3.0;
 
+    cout<<"Automobilis "<<marke<<" "<<modelis<<" yra pagamintas "<<metais<<" metais. Jo motoras "<<litrazas<<" litrazo. Automobilis yra "<<spalva<<" spalvos"<<endl;
+    cout<<" "<<endl;
 
+    //4uzd
+    int a=13,b=5;
+    float c=17.5;
+
+    cout<<a+b-c<<endl;
+    cout<<15/2+c<<endl;
+    cout<<a/static_cast<double>(b)+2*c<<endl;
+    cout<<14%3+6.3+b/a<<endl;
+    cout<<static_cast<int>(c)%5+a-b<<endl;
+    cout<<13.5/2+4.0*3.5+18<<endl;
+    cout<<" "<<endl;
+
+    //5uzd
+    int g=5,h=77,i=9,j=22,k=13;
+
+    cout<<(g+h+c+i+j)/5<<endl;
+    cout<<" "<<endl;
+
+    //6uzd
+    int x=33, p=66,y;
+
+    y=p-x;
+    cout<<x<<" + "<<y<<" = "<<p<<endl;
     return 0;
 }
 
