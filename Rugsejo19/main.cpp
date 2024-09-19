49a77bb323a682112017497004c2539b296dcfaa
Dominik Alencinovič <s057152@ad.viko.lt>
2024-09-19 10:37:07 +0300

    1

---
+++ Rugsejo19/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int choise = 0;
+    cout << "Paprastas meniu pasrinkimas" << endl;
+    cout<<"1 Pirma opcija"<<endl;
+    cout<<"2 Antra opcija"<<endl;
+    cout<<"3 Trecia opcija"<<endl;
+    cout<<"4 Iseiti"<<endl;
+    cout<<"Jusu pasirinkimas"<<endl;
+    cin>> choise;
+
+    switch (choise) {
+        case 1:
+            cout<<"Jus pasirinkote 1 opcija"<<endl;
+        break;
+        case 2:
+            cout<<"Jus pasirinkote 2 opcija"<<endl;
+        break;
+        case 3:
+            cout<<"Jus pasirinkote 3 opcija"<<endl;
+        break;
+        case 4:
+            cout<<"Jus isejote is programos"<<endl;
+        break;
+        default:
+            cout<<"Tokio pasirinkimo nera, bandykite dar karta"<<endl;
+    }
+    return 0;
+}
