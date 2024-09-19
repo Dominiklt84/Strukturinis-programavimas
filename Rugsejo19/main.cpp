13a4bd51def96bc8e960ff32c1246b41ffd85d39
Dominik Alencinovič <s057152@ad.viko.lt>
2024-09-19 11:04:28 +0300

    meniu pvz

--- Rugsejo19/main.cpp
+++ Rugsejo19/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main() {
     int choise = 0;
+    int number1,number2,number3,sum,i;
+
 
 
     while (choise != 4) {
@@ -16,12 +18,27 @@ int main() {
         switch (choise) {
             case 1:
                 cout<<"Jus pasirinkote 1 opcija"<<endl;
+                cout<<"Iveskite pirma skaiciu"<<endl;
+            cin>>number1;
+            cout<<"Iveskite antra skaiciu"<<endl;
+            cin>>number2;
+            sum=number1+number2;
+            cout<<"Ivestu skaiciu suma "<<sum<<endl;
             break;
             case 2:
                 cout<<"Jus pasirinkote 2 opcija"<<endl;
+            while (i<=20) {
+                if(i%2==1) {
+                    cout<<"Skaitmuo "<<i<<endl;
+                }
+                i++;
+            }
             break;
             case 3:
                 cout<<"Jus pasirinkote 3 opcija"<<endl;
+            for (int j=0;j<=20;j++) {
+                cout<<"Skaitmuo"<<j<<endl;
+            }
             break;
             case 4:
                 cout<<"Jus isejote is programos"<<endl;
