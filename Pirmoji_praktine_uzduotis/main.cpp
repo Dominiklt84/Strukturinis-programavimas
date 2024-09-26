d982eadcdff287dda75809114f8b383f8d7ae0c7
Dominik <dominik.alencinovic@stud.viko.lt>
2024-09-26 20:03:58 +0300

    testas

---
+++ Pirmoji_praktine_uzduotis/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int kiek,num;
+    string val;
+    cout<<"Iveskite valiuta (EUR,GBP,USD,INR): "<<endl;
+    cin>>val;
+    cout<<"Iveskite kieki:"<<endl;
+    cin>>kiek;
+    cout<<"Pasirinkite veiksma: "<<endl;
+    cout << "1. Valiutos kurso palyginimas. " << endl;
+    cout << "2. Isigyti valiutos. " << endl;
+    cout << "3. Parduoti valiutą. " << endl;
+    cin>>num;
+    switch (num) {
+        case 1:
+            if(val="EUR") {
+                cout<<
+            }
+    }
+
+
+    return 0;
+}
