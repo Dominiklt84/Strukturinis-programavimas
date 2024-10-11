3e80103c4b8864aeb0ceb12b7a2285cb781809e1
Dominik Alencinovič <s057152@ad.viko.lt>
2024-10-11 09:48:39 +0300

    uptd

--- 2projektas/main.cpp
+++ 2projektas/main.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 bool Balse(char raide);
-int bendaliklis(int skaicius1,int skaicius2,int daliklis);
+int bendaliklis(int skaicius1,int skaicius2);
+
 int main()
 {
-    int num=0,skaicius1,skaicius2,daliklis;
+    int num=0,skaicius1,skaicius2;
     char raide;
     while(num!=5) {
         cout <<"Pasirinkite veiksma: "<<endl;
         cout << "1. Patikrinimas ar raide yra balse. " << endl;
-        cout << "2. Rasti mažiausia bendra dalikli tarp dvieju įvestu skaitmenu. " << endl;
+        cout << "2. Rasti maziausia bendra dalikli tarp dvieju ivestu skaitmenu. " << endl;
         cout << "3. Mini zaidimas. " << endl;
         cout << "4. Skaiciu tvarka. " << endl;
         cout << "5. Iseiti. "<<endl;
@@ -25,7 +25,12 @@ int main()
             }
             break;
             case 2:
-                bendaliklis(skaicius1,skaicius2);
+                cout<<"Iveskite pirma skaitmuo: "<<endl;
+                cin>>skaicius1;
+                cout<<"Iveskite antra skaitmeni: "<<endl;
+                cin>>skaicius2;
+                cout << "Didziausias bendras daliklis: " << bendaliklis(skaicius1, skaicius2) << endl;
+            break;
 
         }
     }
@@ -42,17 +47,12 @@ bool Balse(char raide) {
     }
     return false;
 }
-int bendaliklis(int skaicius1,int skaicius2,int daliklis) {
-    cout<<"Iveskite pirma skaitmuo: "<<endl;
-    cin>>skaicius1;
-    cout<<"Iveskite antra skaitmuo: "<<endl;
-    cin>>skaicius2;
-    if(skaicius1>skaicius2) {
-        daliklis=skaicius1-skaicius2;
-    }else {
-        daliklis= skaicius2-skaicius1;
+
+int bendaliklis(int skaicius1,int skaicius2) {
+    while(skaicius2 !=0) {
+            int daliklis=skaicius2 % skaicius1;
+            skaicius1=skaicius2;
+            skaicius2=daliklis;
     }
- 
-
-
+    return skaicius1;
 }
