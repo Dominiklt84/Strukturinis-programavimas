9a117b228f46e37dff1f631b1e6ed959a91030fc
Dominik Alencinovič <s057152@ad.viko.lt>
2024-11-08 09:54:41 +0200

    uptd

--- trecioji_praktine/main.cpp
+++ trecioji_praktine/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 void Firstprog();
 int main() {
     int num=0;
@@ -17,49 +18,53 @@ int main() {
                 break;
 
         }
-    }
-    */
+    }*/
+
     Firstprog();
 
 
     return 0;
 }
 void Firstprog() {
-    int ABC[32]={'A','Ą','B','C','Č','D','E','Ę','Ė','F','G','H','I','Į','Y','J','K','L','M','N','O','P','R','S','Š','T','U','Ų','Ū','V','Z','Ž'};
-    char text[100]={'0'},key[100]={'0'},word[100]={'0'};
+    const char ABC[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char text[50],key[50],word[50],new_key[50];
     cout<<"Iveskite pradini teksta: "<<endl;
     cin>>text;
     cout<<"Iveskite slapta rakta: "<<endl;
     cin>>key;
-    for(int i=0;i<100;i++) {
-        for(int j=0;j<32;j++) {
-            if(text[i]==ABC[j]) {
+    for(int i=0;i<strlen(text);i++) {
+        new_key[i]=key[i%strlen(key)];
+    }
+    int size_text=strlen(text),size_key=strlen(new_key);
+
+    for(int i=0;i<size_text;i++) {
+        for(int j=0;j<sizeof(ABC);j++) {
+            if(toupper(text[i])==ABC[j]) {
                 text[i]=j;
             }
-            if(key[i]=ABC[j]) {
-                key[i]=j;
+
+            if(toupper(new_key[i])==ABC[j]) {
+                new_key[i]=j;
             }
         }
     }
-    for(int i=0;i<100;i++) {
-        if(text[i]+key[i]<32) {
-            word[i]=text[i]+key[i];
+
+    for(int i=0;i<size_text;i++) {
+        if(text[i]+new_key[i]<sizeof(ABC)) {
+            word[i]=text[i]+new_key[i];
         }else {
-            word[i]=(text[i]+key[i])%32;
+            word[i]=(text[i]+new_key[i])%strlen(ABC);
         }
     }
-    for(int i=0;i<100;i++) {
-        for(int j=0;j<32;j++) {
+    for(int i=0;i<size_text;i++) {
+        for(int j=0;j<sizeof(ABC);j++) {
             if(word[i]==ABC[j]) {
-                word[i]=j;
+                word[i]=ABC[j];
             }
         }
     }
     cout<<"Jusu kodas:"<<endl;
-    for(int i=0;i<100;i++) {
-        cout<<word[i];
-    }
-
+    cout<<word;
 
 
 }
