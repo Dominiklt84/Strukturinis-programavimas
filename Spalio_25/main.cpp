867d52896c06a6fcd9d937db2ff0577bc9511b39
Dominik Alencinovič <s057152@ad.viko.lt>
2024-11-21 11:01:58 +0200

    uptd

---
+++ Spalio_25/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+     //Uzd 9
+    int sum=0;
+    for(int i=1;i<=100;i++) {
+        sum=i+sum;
+    }
+    cout<<"Suma: "<<sum<<endl;
+    //Uzd 10
+    int sk1,sk2,sk,sum;
+    cout<<"Skaicius: "<<endl;
+    cin>>sk;
+    sk1=sk/10;
+    sk2=sk-(sk1*10);
+    sum=sk2*10+sk1;
+    cout<<"Rezultatas: "<<sum<<endl;
+    //Uzd 11
+    for(int i=1;i<=20;i++) {
+        if(i%5==0) {
+            cout<<"-"<<endl;
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    */
+    //Uzd 12
+    int sk;
+    cout<<"Skaicius: "<<endl;
+    cin>>sk;
+    return 0;
+}
